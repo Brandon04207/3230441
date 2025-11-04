a8a6663f9250cc2aee71c16baf9138c0a85282df
Brandon04207 <brandon04207@gmail.com>
2025-11-04 14:02:34 -0500

    numeroParImpar

---
+++ curso de python/ejercicio.py
@@ -0,0 +1,7 @@
+# validar si un numero es par o impar 
+
+numero = int (input("digita numero entero:_"))
+if numero %2 == 0:
+    print ("su numero es par")
+else:
+    print ( "su numero es impar")
